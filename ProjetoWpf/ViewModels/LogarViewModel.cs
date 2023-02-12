118a828537b03a5dbb310afdbd2774b5d0c5a2a9
DnajaAraujo <dnaja.araujo09@gmail.com>
2023-02-12 18:36:37 -0300

    fix: update project

---
+++ ProjetoWpf/ViewModels/LogarViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoWpf.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoWpf.ViewModels
+{
+    public class LogarViewModel
+    {
+        public LogarCommand Logar { get; set; }
+
+        public LogarViewModel()
+        {
+            Logar = new LogarCommand();
+        }
+    }
+}
